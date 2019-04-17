01217fd37a5526a639f105e9197559a1b6939c00
Renann Souza <renann_puera@hotmail.com>
2019-04-17 20:26:03 -0300

    Adiciona test em Home

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """GET / deve retorna status code 200"""
+
+        self.assertEqual(200,self.response.status_code)
+
+
+    def test_template(self):
+        """Deve retorna index.html no response"""
+        self.assertTemplateUsed(self.response, 'index.html')
