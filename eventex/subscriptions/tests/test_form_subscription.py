10e2cd096442074d8b486dc97f31d177b74e5d49
Renann Souza <renann_puera@hotmail.com>
2019-04-22 17:39:28 -0300

    E-mail ou Telefone opcionais

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -27,6 +27,21 @@ class SubscriptionTestForm(TestCase):
         form = self.make_validated_form(name='RENANN souza')
         self.assertEqual('Renann Souza', form.cleaned_data['name'])
 
+    def test_email_is_optional(self):
+        """E-mail é opcional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Dever ser informado e-mail ou telefone"""
+        form = self.make_validated_form(email='',phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
+
+    def test_phone_is_optional(self):
+        """Telefone é opcional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
         errors_list = errors[field]
--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -77,3 +77,11 @@ class SubscriptionsNewPostInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
+
+
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(name='Renann Souza', cpf='12345678912')
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+
+        self.assertContains(response, '<ul class="errorlist nonfield">')
