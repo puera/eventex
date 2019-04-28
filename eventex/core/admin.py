80c33d919be4b3f8c5a6ea4e5173e5fe90be44b3
Renann Souza <renann_puera@hotmail.com>
2019-04-28 14:20:03 -0300

    Managers

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -11,7 +11,7 @@ class ContactInLine(admin.TabularInline):
 class SpeakerModelAdmin(admin.ModelAdmin):
     inlines = [ContactInLine]
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ['name', 'photo_img', 'website_link']
+    list_display = ['name', 'photo_img', 'website_link', 'email', 'phone']
 
     def website_link(self, obj):
         return format_html('<a href="{0}">{0}</a>', obj.website)
@@ -20,6 +20,16 @@ class SpeakerModelAdmin(admin.ModelAdmin):
 
     def photo_img(self, obj):
         return format_html('<img width="32px" src="{}" />', obj.photo)
+
+    def email(self, obj):
+        return obj.contact_set.emails().first()
+
+    email.short_description = 'email'
+
+    def phone(self, obj):
+        return obj.contact_set.emails().first()
+
+    phone.short_description = 'telefone'
 
     photo_img.short_description = 'foto'
 
